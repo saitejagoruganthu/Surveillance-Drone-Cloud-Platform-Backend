46760e603ccc809a19197db4c901065e93fb51fd
Tirumala Saiteja Goruganthu <saiteja.goruganthu117@gmail.com>
2024-04-30 21:22:45 -0700

    first commit

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mongoose=require("mongoose");
 
 //process.env.MONGO_URI = "mongodb+srv://us-west-2.aws.data.mongodb-api.com/app/data-pbmtq/endpoint/data/v1"
-url = process.env.DATABASE_URL;
+url = "mongodb+srv://gaocloudgsr:gaocloudgsr@cluster0.vax6b4a.mongodb.net/?retryWrites=true&w=majority";
 const connectDb=async()=>{
     try{
         const conn= await mongoose.connect(url,{
